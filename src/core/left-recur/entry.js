1bb19bb25552eaed108a479cb03951f59dd4fefe
biuld <xboltk@gmail.com>
2021-02-21 13:18:38 +0800

    optimize left recursion elimination

--- src/core/left-recur/entry.js
+++ src/core/left-recur/entry.js
@@ -9,69 +9,71 @@ function epsilon(NT) {
   return [NT, [""]]
 }
 
-function remove_direct(gram) {
-  gram.NT.forEach(nt => {
-    let [not_rec, rec] = gram.rules(nt)
-      .reduce((acc, rule) => {
-        let [left, [l,]] = rule
+function remove_direct(gram, nt) {
+  let [not_rec, rec] = gram.rules(nt)
+    .reduce((acc, rule) => {
+      let [left, [l,]] = rule
 
-        if (left === l)
-          acc[1].push(rule)
-        else
-          acc[0].push(rule)
+      if (left === l)
+        acc[1].push(rule)
+      else
+        acc[0].push(rule)
 
-        return acc
-      }, [[], []])
+      return acc
+    }, [[], []])
 
-    if (rec.length != 0) {
-      let al = alter(nt)
+  if (rec.length != 0) {
+    let al = alter(nt)
 
-      not_rec.forEach(([, right]) => right.push(al))
-      rec.forEach(rule => {
-        rule[0] = al
-        rule[1].shift()
-        rule[1].push(al)
-      })
+    not_rec.forEach(([, right]) => right.push(al))
+    rec.forEach(rule => {
+      rule[0] = al
+      rule[1].shift()
+      rule[1].push(al)
+    })
 
-      gram.NT.push(al)
-      gram.g.push(epsilon(al))
-    }
-  })
+    gram.NT.push(al)
+    gram.g.push(epsilon(al))
+  }
 }
 
 function eliminate(gram) {
   for (let i = 0; i < gram.NT.length; i++) {
-    gram.rules(gram.NT[i]).forEach(cur => {
-      let not_visit = [cur]
-      let res = []
+    gram
+      .rules(gram.NT[i])
+      .filter(([, [l,]]) => gram.NT.includes(l))
+      .forEach(cur => {
+        let not_visit = [cur]
+        let res = []
 
-      while (not_visit.length != 0) {
-        let h = not_visit.shift()
+        while (not_visit.length != 0) {
+          let h = not_visit.shift()
 
-        let [left, [l, ...right]] = h
+          let [left, [l, ...right]] = h
 
-        if (left === l || gram.T.includes(l)) {
-          res.push(h)
-          continue
+          if (left === l || gram.T.includes(l)) {
+            res.push(h)
+            continue
+          }
+
+          for (let j = 0; j < i; j++) {
+            let aj = gram.NT[j]
+
+            if (aj === l)
+              gram.rules(aj)
+                .map(([, r]) => [left, r.concat(right).filter(e => e != undefined)])
+                .forEach(rule => not_visit.push(rule))
+          }
         }
 
-        for (let j = 0; j < i; j++) {
-          let aj = gram.NT[j]
-
-          if (aj === l)
-            gram.rules(aj)
-              .map(([, r]) => [left, r.concat(right).filter(e => e != undefined)])
-              .forEach(rule => not_visit.push(rule))
+        if (res.length != 0) {
+          gram.remove(cur)
+          res
+            .filter(e => !gram.contains(e))
+            .forEach(e => gram.g.push(e))
+          remove_direct(gram, gram.NT[i])
         }
-      }
-
-      if (res.length != 0) {
-        gram.remove(cur)
-        res.filter(e => !gram.contains(e)).forEach(e => gram.g.push(e))
-      }
-    })
-
-    remove_direct(gram)
+      })
   }
 }
 
